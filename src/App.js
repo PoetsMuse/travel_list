b3bb4ced011b30cb92b874cdaee76f7d586399ef
Kate Glushkova <weirdis1@gmail.com>
2023-11-30 13:21:45 +0200

    Comtrolled elements / onChange

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+ import {useState} from 'react'
+ 
  const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: true },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Socks", quantity: 4, packed: false },
   { id: 3, description: "Charger", quantity: 1, packed: false },
 ];
 
@@ -24,10 +26,35 @@ export default function App() {
   }
 
   function Form(){
+
+    const [description, setDescription] = useState('')
+    const [quantity, setQuantity] = useState(1)
+
+    function handleSubmit(e) {
+      e.preventDefault()
+
+      if(!description) return
+
+      const newItem = {description, quantity, packed: false, id: Date.now() }
+      console.log(newItem)
+
+      setDescription('')
+      setQuantity(1)
+    }
+
     return (
-      <div className="add-form">
+      <form className="add-form" onSubmit={handleSubmit}>
         <h3>What do you need for your 😍 trip?</h3>
-      </div>
+        <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
+          {Array.from({length: 20}, (_, i) => i + 1).map(num => <option value={num} key={num}>{num}</option>)}
+        </select>
+        <input 
+          type="text" 
+          placeholder="Item..." 
+          value={description} 
+          onChange={(e) => setDescription(e.target.value)} />
+        <button>Add</button>
+      </form>
     )
   }
 
@@ -36,7 +63,7 @@ export default function App() {
       <div className="list">
         <ul>
           {initialItems.map(item => 
-            (<Item item={item} />
+            (<Item item={item} key={item.id} />
           ))}
         </ul>
       </div>
