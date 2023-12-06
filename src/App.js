e4cd63c38b8c9b7e6e0f5e55b4a0548f87774079
Kate Glushkova <weirdis1@gmail.com>
2023-12-06 12:34:23 +0200

    Sort and clear

--- src/App.js
+++ src/App.js
@@ -20,11 +20,23 @@ export default function App() {
     )
   }
 
+  function handleClearList() {
+    const confirmed = window.confirm(
+      'Are you sure you want to delete all items?'
+      )
+    if (confirmed) setItems([])
+  }
+
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem} 
+        onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
+      />
       <Stats items={items} />
     </div>
   );
@@ -75,11 +87,22 @@ function Form({onAddItems}) {
   );
 }
 
-function PackingList({items, onDeleteItem, onToggleItem}) {
+function PackingList({items, onDeleteItem, onToggleItem, onClearList}) {
+  const [sortBy, setSortBy] = useState("input")
+
+  let sortedItems;
+  if(sortBy === 'input') sortedItems = items
+  if(sortBy === 'description') 
+    sortedItems = items
+      .slice()
+      .sort((a,b) => a.description.localeCompare(b.description))
+
+  if(sortBy === 'packed') sortedItems = items.slice().sort((a,b)=> Number(a.packed) - Number(b.packed))
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item 
             item={item} 
             onDeleteItem={onDeleteItem} 
@@ -88,6 +111,15 @@ function PackingList({items, onDeleteItem, onToggleItem}) {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={e=> setSortBy(e.target.value)}>
+          <option value='input'>Sort by input order</option>
+          <option value='description'>Sort by description</option>
+          <option value='packed'>Sort by packed status</option>
+        </select>
+
+        <button onClick={onClearList}>Clear List</button>
+      </div>
     </div>
   );
 }
