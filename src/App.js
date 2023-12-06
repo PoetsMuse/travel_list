a840b7537f089f406bda78f4a6b8da67c2ff9cca
Kate Glushkova <weirdis1@gmail.com>
2023-12-06 12:05:02 +0200

    Derived state / adding statistics in footer

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ export default function App() {
       <Logo />
       <Form onAddItems={handleAddItems} />
       <PackingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -44,7 +44,6 @@ function Form({onAddItems}) {
     if (!description) return;
 
     const newItem = { description, quantity, packed: false, id: Date.now() };
-    console.log(newItem);
 
     onAddItems(newItem)
 
@@ -105,10 +104,27 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats( {items}) {
+  if(!items.length)
+   return (
+    <p className="stats">
+      <em>Start adding some items into your packing list 🚀</em>
+    </p>
+  )
+
+  const numItems = items.length
+  const numPacked = items.filter((item) => item.packed).length
+  const percentage = Math.round((numPacked / numItems) * 100)
+
   return (
     <footer className="stats">
-      <em>💼 You have x items on your list, and you already packed x (x%)</em>
+      <em>
+        {
+          percentage === 100 
+            ? "You've got everything! Ready to go! ✈️" 
+            : `💼 You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`
+        }
+      </em>
     </footer>
   );
 }
